da0099b995c017830ffdfadf2d96aaf6e0c4a3b4
King.LF <LimboLiarL@gmail.com>
2020-12-14 22:25:49 +0800

    refactor(night-core) 修改表结构

--- long_night.sql
+++ long_night.sql
@@ -1,160 +1,85 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : localhost_3306
-Source Server Version : 50643
-Source Host           : localhost:3306
-Source Database       : long_night
-
-Target Server Type    : MYSQL
-Target Server Version : 50643
-File Encoding         : 65001
-
-Date: 2020-06-03 21:58:16
-*/
-
 SET FOREIGN_KEY_CHECKS=0;
 
--- ----------------------------
--- Table structure for sys_menu
--- ----------------------------
-DROP TABLE IF EXISTS `sys_menu`;
-CREATE TABLE `sys_menu` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID 说明：自增ID',
-  `menu_name` varchar(20) NOT NULL COMMENT '菜单名称',
-  `menu_url` varchar(50) NOT NULL COMMENT '菜单地址',
-  `menu_type` tinyint(1) unsigned NOT NULL COMMENT '菜单类型 说明：0 表示目录，1 表示菜单',
-  `menu_sort` tinyint(3) unsigned NOT NULL COMMENT '菜单排列顺序',
-  `menu_description` varchar(100) DEFAULT NULL COMMENT '菜单描述',
-  `is_deleted` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否已删除 说明：1 表示删除，0 表示未删除',
-  `gmt_create` datetime NOT NULL COMMENT '创建时间',
-  `gmt_modified` datetime DEFAULT NULL COMMENT '修改时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统菜单表';
-
--- ----------------------------
--- Records of sys_menu
--- ----------------------------
-
--- ----------------------------
--- Table structure for sys_permission
--- ----------------------------
-DROP TABLE IF EXISTS `sys_permission`;
-CREATE TABLE `sys_permission` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID 说明：自增ID',
-  `permission_name` varchar(20) NOT NULL COMMENT '权限名称',
-  `permission_description` varchar(100) DEFAULT NULL COMMENT '权限描述',
-  `permission_url` varchar(50) DEFAULT NULL COMMENT '权限访问路径',
-  `permission_type` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '权限类型 说明：0 表示目录，1 表示菜单，2 表示按钮',
-  `is_deleted` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否已删除 说明：1 表示删除，0 表示未删除',
-  `gmt_create` datetime NOT NULL COMMENT '创建时间',
-  `gmt_modified` datetime DEFAULT NULL COMMENT '修改时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统权限表';
-
--- ----------------------------
--- Records of sys_permission
--- ----------------------------
-
--- ----------------------------
--- Table structure for sys_role
--- ----------------------------
-DROP TABLE IF EXISTS `sys_role`;
-CREATE TABLE `sys_role` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID 说明：自增ID',
-  `role_name` varchar(20) NOT NULL COMMENT '角色名称',
-  `role_description` varchar(100) DEFAULT NULL COMMENT '角色描述',
-  `is_deleted` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否已删除 说明：1 表示删除，0 表示未删除',
-  `gmt_create` datetime NOT NULL COMMENT '创建时间',
-  `gmt_modified` datetime DEFAULT NULL COMMENT '修改时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统角色表';
-
--- ----------------------------
--- Records of sys_role
--- ----------------------------
-
--- ----------------------------
--- Table structure for sys_role_menu
--- ----------------------------
-DROP TABLE IF EXISTS `sys_role_menu`;
-CREATE TABLE `sys_role_menu` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID 说明：自增ID',
-  `role_id` bigint(20) unsigned NOT NULL COMMENT '角色ID',
-  `menu_id` bigint(20) unsigned NOT NULL COMMENT '菜单ID',
-  `is_deleted` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否已删除 说明：1 表示删除，0 表示未删除',
-  `gmt_create` datetime NOT NULL COMMENT '创建时间',
-  `gmt_modified` datetime DEFAULT NULL COMMENT '修改时间',
-  PRIMARY KEY (`id`,`role_id`,`menu_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统角色菜单关联表';
-
--- ----------------------------
--- Records of sys_role_menu
--- ----------------------------
-
--- ----------------------------
--- Table structure for sys_role_permission
--- ----------------------------
-DROP TABLE IF EXISTS `sys_role_permission`;
-CREATE TABLE `sys_role_permission` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID 说明：自增ID',
-  `role_id` bigint(20) unsigned NOT NULL COMMENT '角色ID',
-  `permission_id` bigint(20) unsigned NOT NULL COMMENT '用户ID',
-  `is_deleted` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否已删除 说明：1 表示删除，0 表示未删除',
-  `gmt_create` datetime NOT NULL COMMENT '创建时间',
-  `gmt_modified` datetime DEFAULT NULL COMMENT '修改时间',
-  PRIMARY KEY (`id`,`role_id`,`permission_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统角色权限关联表';
-
--- ----------------------------
--- Records of sys_role_permission
--- ----------------------------
-
--- ----------------------------
--- Table structure for sys_role_user
--- ----------------------------
-DROP TABLE IF EXISTS `sys_role_user`;
-CREATE TABLE `sys_role_user` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID 说明：自增ID',
-  `role_id` bigint(20) unsigned NOT NULL COMMENT '角色ID',
-  `user_id` bigint(20) unsigned NOT NULL COMMENT '用户ID',
-  `is_deleted` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否已删除 说明：1 表示删除，0 表示未删除',
-  `gmt_create` datetime NOT NULL COMMENT '创建时间',
-  `gmt_modified` datetime DEFAULT NULL COMMENT '修改时间',
-  PRIMARY KEY (`id`,`role_id`,`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统角色用户关联表';
-
--- ----------------------------
--- Records of sys_role_user
--- ----------------------------
-
--- ----------------------------
--- Table structure for sys_user
--- ----------------------------
+-- ---------系统用户表-----------
 DROP TABLE IF EXISTS `sys_user`;
-CREATE TABLE `sys_user` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID 说明：自增长ID',
-  `login_account` varchar(20) NOT NULL COMMENT '登录账号',
-  `login_pass` varchar(32) NOT NULL COMMENT '登录密码',
-  `login_salt` varchar(32) NOT NULL COMMENT '密码盐',
-  `user_name` varchar(20) NOT NULL COMMENT '昵称',
-  `user_head` varchar(64) DEFAULT NULL COMMENT '头像',
-  `user_phone` varchar(20) DEFAULT NULL COMMENT '手机',
-  `user_email` varchar(30) DEFAULT NULL COMMENT '邮箱',
-  `user_sex` tinyint(1) unsigned DEFAULT NULL COMMENT '性别 说明：1 表示男，0 表示女',
-  `user_type` tinyint(1) unsigned DEFAULT '0' COMMENT '用户类型 说明：1 系统管理员 ，0 普通用户',
-  `login_status` tinyint(1) unsigned DEFAULT '0' COMMENT '登录状态 说明：1 表示已登录,0 表示未登录',
-  `last_login_time` datetime DEFAULT NULL COMMENT '最后登录时间',
-  `last_login_ip` varchar(50) DEFAULT NULL COMMENT '最后登录IP',
-  `is_locked` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否已锁定 说明：1 表示已锁定，0 表示未锁定',
-  `is_deleted` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否已删除 说明：1 表示删除，0 表示未删除',
-  `gmt_create` datetime NOT NULL COMMENT '创建时间',
-  `gmt_modified` datetime DEFAULT NULL COMMENT '修改时间',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uk_login_account` (`login_account`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='系统用户表';
+CREATE TABLE sys_user (
+id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增ID 说明：自增长ID',
+login_account VARCHAR(20) NOT NULL COMMENT '登录账号',
+login_pass VARCHAR(32) NOT NULL COMMENT '登录密码',
+login_salt VARCHAR(32) NOT NULL COMMENT '密码盐',
+user_name VARCHAR(20) NOT NULL COMMENT '昵称',
+user_head VARCHAR(64) NULL COMMENT '头像',
+user_phone VARCHAR(20) NULL COMMENT '手机',
+user_email VARCHAR(30) NULL COMMENT '邮箱',
+user_sex TINYINT(1) UNSIGNED NULL COMMENT '性别 说明：1 表示男，0 表示女',
+user_type TINYINT(1) UNSIGNED NULL DEFAULT 0 COMMENT '用户类型 说明：1 系统管理员 ，0 普通用户',
+is_locked TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否已锁定 说明：1 表示已锁定，0 表示未锁定',
+is_deleted TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否已删除 说明：1 表示删除，0 表示未删除',
+gmt_create DATETIME NOT NULL COMMENT '创建时间',
+gmt_modified DATETIME NULL COMMENT '修改时间',
+PRIMARY KEY (id),
+UNIQUE INDEX uk_sys_user1(login_account) USING BTREE
+)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT '系统用户表';
 
--- ----------------------------
--- Records of sys_user
--- ----------------------------
-INSERT INTO `sys_user` VALUES ('1', 'admin', 'c22e1e2404b7914198773a88dc6272ef', 'admin', '管理员', null, null, null, null, '0', '0', '2020-06-01 23:28:33', '127.0.0.1', '0', '0', '2020-05-16 17:13:39', '2020-06-01 23:28:33');
+-- ---------系统角色表-----------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE sys_role (
+id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增ID 说明：自增ID',
+name VARCHAR(20) NOT NULL COMMENT '角色名称',
+description VARCHAR(100) NULL COMMENT '角色描述',
+is_auto_generation TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否自动生成 说明：1 表示是，0 表示否',
+is_deleted TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否已删除 说明：1 表示删除，0 表示未删除',
+gmt_create DATETIME NOT NULL COMMENT '创建时间',
+gmt_modified DATETIME NULL COMMENT '修改时间',
+PRIMARY KEY (id),
+UNIQUE INDEX uk_sys_role1(name) USING BTREE
+)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT '系统角色表';
+
+-- ---------系统资源表-----------
+DROP TABLE IF EXISTS `sys_resource`;
+CREATE TABLE sys_resource (
+id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增ID 说明：自增ID',
+parent_id BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '父节点资源ID 说明：父节点资源ID,0为根节点',
+name VARCHAR(20) NOT NULL COMMENT '资源名称',
+url VARCHAR(50) NOT NULL COMMENT '请求地址',
+url_type VARCHAR(10) NULL COMMENT '请求类型 说明：GET POST DELETE',
+type TINYINT(1) UNSIGNED NOT NULL COMMENT '资源类型 说明：0 表示菜单，1 表示资源',
+route VARCHAR(50) NULL COMMENT '资源路由',
+sort TINYINT(3) UNSIGNED NOT NULL COMMENT '资源排列顺序',
+parents VARCHAR(20) NOT NULL DEFAULT '0' COMMENT '包含的父节点资源ID 说明：从上到下，用,分隔',
+description VARCHAR(100) NULL COMMENT '资源描述',
+is_auto_generation TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否自动生成 说明：1 表示是，0 表示否',
+is_deleted TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否已删除 说明：1 表示删除，0 表示未删除',
+gmt_create DATETIME NOT NULL COMMENT '创建时间',
+gmt_modified DATETIME NULL COMMENT '修改时间',
+PRIMARY KEY (id)
+)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT '系统资源表';
+
+-- ---------系统角色用户关联表-----------
+DROP TABLE IF EXISTS `sys_role_user`;
+CREATE TABLE sys_role_user (
+id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增ID 说明：自增ID',
+role_id BIGINT(20) UNSIGNED NOT NULL COMMENT '角色ID',
+user_id BIGINT(20) UNSIGNED NOT NULL COMMENT '用户ID',
+is_deleted TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否已删除 说明：1 表示删除，0 表示未删除',
+gmt_create DATETIME NOT NULL COMMENT '创建时间',
+gmt_modified DATETIME NULL COMMENT '修改时间',
+PRIMARY KEY (id),
+UNIQUE INDEX uk_sys_role_user1(role_id,user_id) USING BTREE
+)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT '系统角色用户关联表';
+
+-- ---------系统角色资源关联表-----------
+DROP TABLE IF EXISTS `sys_role_resource`;
+CREATE TABLE sys_role_resource (
+id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增ID 说明：自增ID',
+role_id BIGINT(20) UNSIGNED NOT NULL COMMENT '角色ID',
+resource_id BIGINT(20) UNSIGNED NOT NULL COMMENT '资源ID',
+is_deleted TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否已删除 说明：1 表示删除，0 表示未删除',
+gmt_create DATETIME NOT NULL COMMENT '创建时间',
+gmt_modified DATETIME NULL COMMENT '修改时间',
+PRIMARY KEY (id),
+UNIQUE INDEX uk_sys_role_resource1(role_id,resource_id) USING BTREE
+)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT '系统角色资源关联表';
+
+INSERT INTO `long_night`.`sys_user` (`id`, `login_account`, `login_pass`, `login_salt`, `user_name`, `user_head`, `user_phone`, `user_email`, `user_sex`, `user_type`, `is_locked`, `is_deleted`, `gmt_create`, `gmt_modified`)
+VALUES ('1', 'admin', 'c22e1e2404b7914198773a88dc6272ef', 'admin', '管理员', NULL, NULL, NULL, NULL, '0', '0', '0', '2020-12-14 22:24:17', '2020-12-14 22:24:19');
