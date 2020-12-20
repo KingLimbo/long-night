4f42d30efc9d37145ee49d04f2643d1ce9f7ec5e
King.LF <LimboLiarL@gmail.com>
2020-12-20 16:05:55 +0800

    refactor(generator) 修复生成model不显示db注释的问题

--- night-generator/src/main/java/com/limbo/practice/Generator.java
+++ night-generator/src/main/java/com/limbo/practice/Generator.java
@@ -1,20 +1,31 @@
 package com.limbo.practice;
 
+import org.apache.log4j.Logger;
 import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.api.ProgressCallback;
+import org.mybatis.generator.api.ShellCallback;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 /**
- * Created by laifan on 2020/12/16.
+ *
+ * @author laifan
+ * @date 2020/12/16
  */
-public class Generator {
+public class Generator{
 
+    private static Logger log = Logger.getLogger(Generator.class);
     public static void main(String[] args) throws Exception{
         List<String> warnings = new ArrayList<String>();
         boolean overwrite = true;
@@ -24,5 +35,11 @@ public class Generator {
         DefaultShellCallback callback = new DefaultShellCallback(overwrite);
         MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
         myBatisGenerator.generate(null);
+        Iterator<String> iterator = warnings.iterator();
+        // 打印日志
+        while(iterator.hasNext()) {
+            log.warn(iterator.next());
+        }
+
     }
 }
